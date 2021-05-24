5fd40ce57326f6dfb6ad3083eaf6dacb71598dcb
titaniumbutter <israelfranco1998@gmail.com>
2021-05-24 15:57:36 -0500

    This is my first calculator

--- calc.js
+++ calc.js
@@ -7,12 +7,36 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? ", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+
+	console.log(`mathSymbol ${mathSymbol}`);
+	console.log(`num1 ${num1}`);
+	console.log(`num2 ${num2}`);
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}
 
 
 	// This line closes the connection to the command line interface.
